e3e60a129101eae3f9cb24be816fbb0010ec810c
jaxs-ribs <luc.baracat@googlemail.com>
2024-07-16 16:30:45 +0200

    Register api key function blocking

---
+++ stt_api/src/lib.rs
@@ -0,0 +1,38 @@
+use crate::exports::kinode::process::stt::{Guest, SttRequest, SttResponse};
+use kinode_process_lib::{vfs, Request, Response};
+
+wit_bindgen::generate!({
+    path: "target/wit",
+    world: "stt-uncentered-dot-os-api-v0",
+    generate_unused_types: true,
+    additional_derives: [serde::Deserialize, serde::Serialize, process_macros::SerdeJsonInto],
+});
+
+// TODO: Zena: Should we maybe activate inherit(true) and explicitly send a response on fail?
+fn register_api_key(key: String) -> anyhow::Result<Result<String, String>> {
+    let SttResponse::RegisterApiKey(result) = Request::new()
+        .target(("our", "stt", "command_center", "uncentered.os"))
+        .body(SttRequest::RegisterApiKey(key))
+        .send_and_await_response(5)??
+        .body()
+        .try_into()?
+    else {
+        return Err(anyhow::anyhow!("Failed to register API key"));
+    };
+    Ok(result)
+}
+
+struct Api;
+impl Guest for Api {
+    fn register_api_key(key: String) -> Result<String, String> {
+        match register_api_key(key) {
+            Ok(result) => result,
+            Err(e) => Err(format!("{e:?}")),
+        }
+    }
+
+    fn openai_transcribe(audio: Vec<u8>) -> Result<String, String> {
+        todo!()
+    }
+}
+export!(Api);
