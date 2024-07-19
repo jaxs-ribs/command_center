9c918523e19ddfd0a7d709dc92faf3ae213475fd
jaxs-ribs <luc.baracat@googlemail.com>
2024-07-19 14:39:15 +0200

    Tg function api ready?

--- tg_api/src/lib.rs
+++ tg_api/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::exports::kinode::process::tg::{Guest, TgRequest, TgResponse, SendMessageParams};
+use crate::exports::kinode::process::tg::{Guest, SendMessageParams, TgRequest, TgResponse};
 use kinode_process_lib::Request;
 
 wit_bindgen::generate!({
@@ -8,27 +8,53 @@ wit_bindgen::generate!({
     additional_derives: [serde::Deserialize, serde::Serialize, process_macros::SerdeJsonInto],
 });
 
+fn send_tg_request(request: TgRequest) -> anyhow::Result<TgResponse> {
+    Request::new()
+        .target(("our", "tg", "command_center", "uncentered.os"))
+        .body(request)
+        .send_and_await_response(5)??
+        .body()
+        .try_into()
+        .map_err(|_| anyhow::anyhow!("Failed to convert response"))
+}
+
+fn handle_tg_response<T>(response: anyhow::Result<TgResponse>, expected: fn(TgResponse) -> Option<Result<T, String>>) -> Result<T, String> {
+    match response {
+        Ok(resp) => expected(resp).unwrap_or_else(|| Err("Unexpected response type".to_string())),
+        Err(e) => Err(format!("{e:?}")),
+    }
+}
 
 struct Api;
 impl Guest for Api {
     fn register_token(token: String) -> Result<(), String> {
-        todo!()
+        handle_tg_response(send_tg_request(TgRequest::RegisterToken(token)), |r| {
+            if let TgResponse::RegisterToken(result) = r { Some(result) } else { None }
+        })
     }
 
     fn subscribe() -> Result<(), String> {
-        todo!()
+        handle_tg_response(send_tg_request(TgRequest::Subscribe), |r| {
+            if let TgResponse::Subscribe(result) = r { Some(result) } else { None }
+        })
     }
 
     fn unsubscribe() -> Result<(), String> {
-        todo!()
+        handle_tg_response(send_tg_request(TgRequest::Unsubscribe), |r| {
+            if let TgResponse::Unsubscribe(result) = r { Some(result) } else { None }
+        })
     }
 
     fn get_file(file_id: String) -> Result<(), String> {
-        todo!()
+        handle_tg_response(send_tg_request(TgRequest::GetFile(file_id)), |r| {
+            if let TgResponse::GetFile(result) = r { Some(result) } else { None }
+        })
     }
 
     fn send_message(params: SendMessageParams) -> Result<(), String> {
-        todo!()
+        handle_tg_response(send_tg_request(TgRequest::SendMessage(params)), |r| {
+            if let TgResponse::SendMessage(result) = r { Some(result) } else { None }
+        })
     }
 }
 export!(Api);
