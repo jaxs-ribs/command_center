6f497a263599ec393f8a756b537adae0db7d9d42
jaxs-ribs <luc.baracat@googlemail.com>
2024-08-21 19:40:59 +0200

    wip

--- command_center/src/lib.rs
+++ command_center/src/lib.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
 use crate::kinode::process::llm::{register_groq_api_key, register_openai_api_key};
 use crate::kinode::process::embedding::{EmbeddingRequest, EmbeddingResponse};
-use kinode_process_lib::{await_message, call_init, get_blob, println, Address, Message};
+use kinode_process_lib::{await_message, call_init, get_blob, get_typed_state, println, Address, Message};
+use sha2::{Sha256, Digest};
 
 const OPENAI_API_KEY: &str = include_str!("../../OPENAI_API_KEY");
 
@@ -11,26 +14,57 @@ wit_bindgen::generate!({
     additional_derives: [serde::Deserialize, serde::Serialize, process_macros::SerdeJsonInto],
 });
 
-pub struct State {
+pub type ContentHash = String;
+pub type Content = String;
+pub type Embedding = Vec<f32>;
 
+#[derive(serde::Serialize, serde::Deserialize, Clone, Default, Debug)]
+pub struct State {
+    pub master_hash_map: HashMap<ContentHash, Embedding>,
+    pub incoming_hashes: Vec<ContentHash>,
+    pub new_hashes: Vec<ContentHash>,
+    pub content_to_embed: Vec<Content>,
 }
 
-fn handle_request(body: &[u8]) -> Result<(), String> {
+fn handle_request(state: &mut State, body: &[u8]) -> anyhow::Result<()> {
     let request: EmbeddingRequest = serde_json::from_slice(body)?;
     match request {
-        EmbeddingRequest::GetEmbeddingsForTexts(texts) => get_embeddigns_for_texts(texts),
+        EmbeddingRequest::GetEmbeddingsForTexts(texts) => handle_embedding_request(state, texts),
     }
+}
+
+fn handle_embedding_request(state: &mut State, texts: Vec<String>) -> anyhow::Result<()> {
+    for text in &texts {
+        let content_hash = content_hash(text);
+        state.incoming_hashes.push(content_hash.clone());
+        if state.master_hash_map.contains_key(&content_hash) {
+            state.new_hashes.push(content_hash);
+            state.content_to_embed.push(text.clone());
+        }
+
+        // let new_embeddings = get_embeddings();
+        
+
+    }
+
+
+
+    // TODO: Send response
     Ok(())
 }
 
-fn get_embeddigns_for_texts(texts: Vec<String>) -> Result<Vec<Vec<f32>>, String> {
 
+fn content_hash(content: &Content) -> ContentHash {
+    let mut hasher = Sha256::new();
+    hasher.update(content.as_bytes());
+    format!("{:x}", hasher.finalize())
 }
 
-fn handle_message(_our: &Address) -> anyhow::Result<()> {
+
+fn handle_message(state: &mut State, _our: &Address) -> anyhow::Result<()> {
     let message = await_message()?;
     if let Message::Request { body, .. } = message {
-        handle_request(&body)?;
+        handle_request(state, &body)?;
     }
     Ok(())
 }
@@ -40,8 +74,11 @@ fn init(our: Address) {
     println!("Starting command centers embedding engine");
     println!("{:?}", register_openai_api_key(OPENAI_API_KEY).unwrap());
 
+    let mut state: State =
+        get_typed_state(|bytes| Ok(bincode::deserialize(bytes)?)).unwrap_or_default();
+
     loop {
-        if let Err(e) = handle_message(&our) {
+        if let Err(e) = handle_message(&mut state, &our) {
             println!("Error: {:?}", e);
         }
     }
