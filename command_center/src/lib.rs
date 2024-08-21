1586e10d0da930b4ca10d21f25815716fb0e2975
jaxs-ribs <luc.baracat@googlemail.com>
2024-08-21 19:54:55 +0200

    ready for testing

--- command_center/src/lib.rs
+++ command_center/src/lib.rs
@@ -1,9 +1,11 @@
 use std::collections::HashMap;
 
-use crate::kinode::process::llm::{register_groq_api_key, register_openai_api_key, embedding};
 use crate::kinode::process::embedding::{EmbeddingRequest, EmbeddingResponse};
-use kinode_process_lib::{await_message, call_init, get_blob, get_typed_state, println, Address, Message};
-use sha2::{Sha256, Digest};
+use crate::kinode::process::llm::{embedding, register_openai_api_key};
+use kinode_process_lib::{
+    await_message, call_init, get_typed_state, println, Address, Message, Response
+};
+use sha2::{Digest, Sha256};
 
 const OPENAI_API_KEY: &str = include_str!("../../OPENAI_API_KEY");
 
@@ -21,6 +23,7 @@ pub type Embedding = Vec<f32>;
 #[derive(serde::Serialize, serde::Deserialize, Clone, Default, Debug)]
 pub struct State {
     pub master_hash_map: HashMap<ContentHash, Embedding>,
+
     pub incoming_hashes: Vec<ContentHash>,
     pub new_hashes: Vec<ContentHash>,
     pub content_to_embed: Vec<Content>,
@@ -41,25 +44,42 @@ fn handle_embedding_request(state: &mut State, texts: Vec<String>) -> anyhow::Re
             state.new_hashes.push(content_hash);
             state.content_to_embed.push(text.clone());
         }
-
-        let new_embeddings = embedding(&texts, None);
-        
-
     }
 
+    let Ok(new_embeddings) = embedding(&state.content_to_embed, None) else {
+        return Err(anyhow::anyhow!("Failed to get embeddings"));
+    };
+
+    assert_eq!(state.new_hashes.len(), new_embeddings.len());
+    for (hash, embedding) in state.new_hashes.iter().zip(new_embeddings.iter()) {
+        state
+            .master_hash_map
+            .insert(hash.clone(), embedding.clone());
+    }
+
+    let mut return_list = Vec::new();
+    for hash in state.incoming_hashes.iter() {
+        return_list.push(state.master_hash_map.get(hash).unwrap().clone());
+    }
+
+    state.incoming_hashes.clear();
+    state.new_hashes.clear();
+    state.content_to_embed.clear();
+
+    let response  = EmbeddingResponse::GetEmbeddingsForTexts(Ok(return_list));
+    Response::new()
+        .body(serde_json::to_vec(&response)?)
+        .send()?;
 
 
-    // TODO: Send response
     Ok(())
 }
-
 
 fn content_hash(content: &Content) -> ContentHash {
     let mut hasher = Sha256::new();
     hasher.update(content.as_bytes());
     format!("{:x}", hasher.finalize())
 }
-
 
 fn handle_message(state: &mut State, _our: &Address) -> anyhow::Result<()> {
     let message = await_message()?;
